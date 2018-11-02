b272c80bb2fc15342720231280c3e9941f5dcdba
Oscar <o@ovl.design>
2018-11-02 22:23:21 +0100

    rmv console

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import * as contentful from 'contentful'
 
-console.log(process.env.cfSpace)
-
 const config = {
   space: process.env.CF_SPACE || process.env.cfSpace,
   accessToken: process.env.CF_TOKEN || process.env.cfToken
