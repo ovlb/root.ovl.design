62de96bd43bd636944807d7979d8e412be14db53
Oscar <o@ovl.design>
2018-11-02 18:05:07 +0100

    add site footer

---
+++ components/global/TheFooter.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="site-footer">
+    <section>
+      <h3 class="sub-headline u-is-visually-hidden">Kontakt</h3>
+      <p class="name">
+        <span>Oscar Braunert</span>
+        <span>Kiefholzstraße 16</span>
+        <span>12435 Berlin</span>
+
+      </p>
+    </section>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+@import '~assets/css/sass/import';
+
+.site-footer {
+  border-top: 1px solid #666;
+  font-size: var(--type-small);
+  margin-top: 4vmin;
+  padding: 2vmin;
+  text-align: center;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,20 +2,21 @@
   <div class="default-layout">
     <main-header />
     <nuxt/>
+    <the-footer />
   </div>
 </template>
 
 <script>
 import MainHeader from '~/components/MainHeader'
-// import TheFooter from '~/components/TheFooter.vue'
+import TheFooter from '~/components/global/TheFooter.vue'
 
 export default {
   metaInfo: {
     // titleTemplate: `%s | ${this.$store.state.siteName}`
   },
   components: {
-    MainHeader
-    // TheFooter
+    MainHeader,
+    TheFooter
   }
 }
 </script>
